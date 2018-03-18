88e190ecb8f31d2137390440392fee6d601f1146
Tobias Mayer <tobim@fastmail.fm>
2018-03-18 21:38:55 +0100

    More descriptive output.

--- example/main.cpp
+++ example/main.cpp
@@ -4,6 +4,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "build: " << platform::compiler << '\n'
-              << "dependency: " << platform::link::compiler << '\n';
+    std::cout << "compiler_id of current translation: " << platform::compiler << '\n'
+              << "compiler_id of link target:         " << platform::link::compiler << '\n';
 }
